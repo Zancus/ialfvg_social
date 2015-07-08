3bebb99e90b3ba3b19adf15f2a5b421296ed4914
Zanca <Zanca@example.com>
2015-07-08 11:26:57 +0200

    users are ready

--- src/it/ialweb/poi/users/UsersFragment.java
+++ src/it/ialweb/poi/users/UsersFragment.java
@@ -1,5 +1,9 @@
 package it.ialweb.poi.users;
 
+import java.util.ArrayList;
+
+import com.shephertz.app42.paas.sdk.android.user.User;
+
 import it.ialweb.poi.R;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -14,6 +18,7 @@ public class UsersFragment extends Fragment {
 
 	private RecyclerView rvusers;
 	private UserAdapter useradapter;
+	private ArrayList<User> users = new ArrayList<User>();
 	
 	public static UsersFragment newInstance()
 	{
@@ -25,10 +30,17 @@ public class UsersFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View users = inflater.inflate(R.layout.fragment_users, null);
-		rvusers = (RecyclerView) users.findViewById(R.id.rvusers);
+		View viewusers = inflater.inflate(R.layout.fragment_users, null);
+		rvusers = (RecyclerView) viewusers.findViewById(R.id.rvusers);
 		LinearLayoutManager llm = new LinearLayoutManager(getActivity());
-		return users;
+		rvusers.setLayoutManager(llm);
+		getUsers();
+		useradapter = new UserAdapter(getActivity(), users);
+		return viewusers;
 	}
 	
+	private void getUsers()
+	{
+		
+	}
 }
