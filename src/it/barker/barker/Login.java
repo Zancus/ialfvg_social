e73ff2d3b3c254c4d9449d02737400b47aa73c4b
EfremFavetta <efrem.favetta@gmail.com>
2015-07-09 11:25:41 +0200

    as

--- src/it/barker/barker/Login.java
+++ src/it/barker/barker/Login.java
@@ -2,12 +2,15 @@ package it.barker.barker;
 
 import it.barker.R;
 
+import com.shephertz.app42.paas.sdk.android.App42API;
 import com.shephertz.app42.paas.sdk.android.App42CallBack;
 import com.shephertz.app42.paas.sdk.android.App42Exception;
 import com.shephertz.app42.paas.sdk.android.user.User;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +20,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends Activity {
+	protected static final String USER = null;
+	protected static final String PASSWORD = null;
 	Button bLogin, bRegistration;
 	EditText eUser, ePass;
 	
@@ -24,6 +29,14 @@ public class Login extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		SharedPreferences pref = getSharedPreferences("it.barker.barker", Context.MODE_PRIVATE);
+		String u = pref.getString("USER", null);
+		String p = pref.getString("PASSWORD", null);
+
+		if(!u.equals(null) && !p.equals(null))
+			DoLogin(u, p);
+		
 		bLogin = (Button)findViewById(R.id.bLogin);
 		bRegistration = (Button)findViewById(R.id.bRegistration);
 		eUser = (EditText)findViewById(R.id.eUsername);
@@ -54,7 +67,7 @@ public class Login extends Activity {
 		});
 	}//create
 	
-	private void DoLogin(String u, String p){
+	private void DoLogin(final String u, final String p){
 		
 		final ProgressDialog spinner = new ProgressDialog(this);
 		spinner.setTitle("Loading..");
@@ -64,6 +77,11 @@ public class Login extends Activity {
 		public void onSuccess(Object response)
 		{	
 			spinner.dismiss();
+			
+			SharedPreferences pref = getSharedPreferences("it.barker.barker", Context.MODE_PRIVATE);
+			pref.edit().putString(USER, u);
+			pref.edit().putString(PASSWORD, p);
+			App42API.setLoggedInUser(u);
 			
 			Intent vIntent = new Intent(Login.this, MainActivity.class);
 			startActivity(vIntent);
